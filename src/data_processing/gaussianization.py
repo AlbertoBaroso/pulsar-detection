64a16e29d91b4b5bda1aa96cc603da2f733ca7fb
Alberto Baroso <albertobaroso999@gmail.com>
2022-11-18 16:23:11 +0100

    Improved code documentation for data processing III

--- src/data_processing/gaussianization.py
+++ src/data_processing/gaussianization.py
@@ -3,53 +3,72 @@ import numpy
 
 ### GAUSSIANIZATION ###
 
-# Compute percentage of samples smaller than x
-def rank_samples(feature, training_samples):
+
+def rank_samples(feature: numpy.ndarray, training_samples: Optional[numpy.ndarray]) -> list[float]:
+    """
+        Compute, for each value, the percentage of values smaller than the current value
+
+        Parameters
+        ----------
+        feature (numpy.ndarray): feature to rank
+        training_samples (Optional[numpy.ndarray]): training samples to rank against
+    """
+
     comparison_feature = feature if training_samples is None else training_samples
     ranks = []
     for x in feature:
         less_than_x = 0
-        for x_i in comparison_feature: # x_i is the value of the considered feature for the i-th training sample
+        for x_i in comparison_feature:  # x_i is the value of the considered feature for the i-th training sample
             if x_i < x:
                 less_than_x += 1
         rank = (less_than_x + 1) / (feature.shape[0] + 2)
+        # +2 because we assume the existance of a feature smaller than all the others and a feature larger than all the others)
         ranks.append(rank)
-    # +2 because we assume the existance of a feature smaller than all the others and a feature larger than all the others)
     return ranks
 
-# Compute percentile (percent poin function) of the rank
-def rank_percentile(rank):
+
+def rank_percentile(rank: list[float]) -> numpy.ndarray:
+    """ Compute percentile (percent poin function) of the rank """
     return norm.ppf(rank)
 
-def gaussianize_feature(feature, training_samples=None):
+
+def gaussianize_feature(feature, training_samples=None) -> numpy.ndarray:
+    """ Transform a feature into a gaussian distribution """
     return numpy.array(rank_percentile(rank_samples(feature, training_samples)))
-    
-def gaussianize_training_samples(DTR):
+
+
+def gaussianize_training_samples(DTR: numpy.ndarray) -> numpy.ndarray:
+    """ Transform each feature of the training samples into a gaussian distribution """
     return numpy.array([gaussianize_feature(x) for x in DTR])
 
-def gaussianize_test_samples(DTE, DTR):
-    # Compute gaussianization by raking test samples against training samples
+
+def gaussianize_test_samples(DTE: numpy.ndarray, DTR: numpy.ndarray) -> numpy.ndarray:
+    """ Compute gaussianization by raking test samples against training samples """
     result = []
     for i in range(len(DTE)):
         result.append(gaussianize_feature(DTE[i], DTR[i]))
     return numpy.vstack(result)
-    
-if __name__ == '__main__': 
-    
+
+
+if __name__ == '__main__':
+
     import sys
     sys.path.append("./")
     from data_processing.data_load import load_training, load_features, load_test
     from visualization.feature_plots import plot_feature_histogram
-    
+
     features = load_features()
     training_samples, training_labels = load_training()
     test_samples, test_labels = load_test()
-    
+
     # GAUSSIANIZATION OF SAMPLES #
-    gaussianized_training_samples = gaussianize_training_samples(training_samples)
-    gaussianized_test_samples = gaussianize_test_samples(test_samples, training_samples)
-    
+    gaussianized_training_samples = gaussianize_training_samples(
+        training_samples)
+    gaussianized_test_samples = gaussianize_test_samples(
+        test_samples, training_samples)
+
     for i in range(len(features)):
-        plot_feature_histogram(features[i], gaussianized_training_samples[i], training_labels)
-        plot_feature_histogram(features[i], gaussianized_test_samples[i], test_labels)
-        
+        plot_feature_histogram(
+            features[i], gaussianized_training_samples[i], training_labels)
+        plot_feature_histogram(
+            features[i], gaussianized_test_samples[i], test_labels)
--- src/visualization/feature_plots.py
+++ src/visualization/feature_plots.py
@@ -1,10 +1,20 @@
-import matplotlib.pyplot as plt
 from data_processing.data_load import load_training, load_features
 from data_processing.analytics import pearson_correlation, z_normalization
 from data_processing.dimensionality_reduction import pca
+import matplotlib.pyplot as plt
+import numpy
 
-# Plot histogram for all features
-def plot_features_histograms(features, samples, labels):
+
+def plot_features_histograms(features: list[str], samples: numpy.ndarray, labels: numpy.ndarray) -> None:
+    """
+        Plot histogram for all features
+
+        Parameters
+        ----------
+        features (list[str]): array of features names
+        samples (numpy.ndarray): array of samples
+        labels (numpy.ndarray): array of numerical labels
+    """
 
     # Get samples by class
     non_pulsars = samples[:, labels == 0]
@@ -12,27 +22,48 @@ def plot_features_histograms(features, samples, labels):
 
     # Plot histogram
     for i in range(len(features)):
-        plt.hist(non_pulsars[i], bins=20, density=True, label="Non pulsars", alpha=0.4)
+        plt.hist(non_pulsars[i], bins=20, density=True,
+                 label="Non pulsars", alpha=0.4)
         plt.hist(pulsars[i], bins=20, density=True, label="Pulsars", alpha=0.4)
         plt.xlabel(features[i])
         plt.legend()
         plt.show()
 
-# Plot histogram for only one feature
-def plot_feature_histogram(feature, samples, labels):
+
+def plot_feature_histogram(feature: str, samples: numpy.ndarray, labels: numpy.ndarray) -> None:
+    """
+        Plot histogram for only one feature
+
+        Parameters
+        ----------
+        features (str): features name
+        samples (numpy.ndarray): array of samples
+        labels (numpy.ndarray): array of numerical labels
+    """
 
     # Get samples by class
     non_pulsars = samples[labels == 0]
     pulsars = samples[labels == 1]
 
     # Plot histogram
-    plt.hist(non_pulsars, bins=50, density=True, label="Non pulsars", alpha=0.4)
+    plt.hist(non_pulsars, bins=50, density=True,
+             label="Non pulsars", alpha=0.4)
     plt.hist(pulsars, bins=50, density=True, label="Pulsars", alpha=0.4)
     plt.xlabel(feature)
     plt.legend()
     plt.show()
 
-def plot_feature_pairs_sctterplots(features, samples, labels):
+
+def plot_feature_pairs_sctterplots(features: list[str], samples: numpy.ndarray, labels: numpy.ndarray) -> None:
+    """
+        Plot scatterplot for each pair of features
+
+        Parameters
+        ----------
+        features (list[str]): array of features names
+        samples (numpy.ndarray): array of samples
+        labels (numpy.ndarray): array of numerical labels
+    """
 
     # Get samples by class
     non_pulsars = samples[:, labels == 0]
@@ -43,25 +74,35 @@ def plot_feature_pairs_sctterplots(features, samples, labels):
         for j in range(len(features)):
             if i != j:
 
-                plt.plot(non_pulsars[i], non_pulsars[j], linestyle='', marker='.', markersize=10, label="Non pulsars")
-                plt.plot(pulsars[i], pulsars[j], linestyle='', marker='.', markersize=10, label="Pulsars")
+                plt.plot(non_pulsars[i], non_pulsars[j], linestyle='',
+                         marker='.', markersize=10, label="Non pulsars")
+                plt.plot(pulsars[i], pulsars[j], linestyle='',
+                         marker='.', markersize=10, label="Pulsars")
                 plt.xlabel(features[i])
                 plt.ylabel(features[j])
-                
+
                 plt.legend()
                 plt.show()
 
 
-def plot_correlation_heatmap(samples):
+def plot_correlation_heatmap(samples: numpy.ndarray) -> None:
+    """
+        Plot heatmap of pearson correlation between features
+
+        Parameters
+        ----------
+        samples (numpy.ndarray): array of samples
+    """
+    
     pearson_matrix = pearson_correlation(samples)
     plt.imshow(pearson_matrix, cmap='gist_yarg', vmin=0, vmax=1)
     # Add correlation value on each square of the heatmap
     for x in range(len(pearson_matrix)):
         for y in range(len(pearson_matrix[x])):
             cor_color = 'w' if pearson_matrix[x][y] > 0.5 else 'k'
-            plt.text(x, y, '%.2f' % pearson_matrix[x][y], color=cor_color, ha='center', va='center')
+            plt.text(
+                x, y, '%.2f' % pearson_matrix[x][y], color=cor_color, ha='center', va='center')
     plt.show()
-    
 
 
 if __name__ == '__main__':
@@ -69,17 +110,17 @@ if __name__ == '__main__':
     # Load data from file
     samples, labels = load_training()
     features = load_features()
-    
+
     z_normalized_samples = z_normalization(samples)
-    pca_samples = pca(samples, 4, labels)
-    
+    pca_samples = pca(samples, 4)
+
     # Correlation heatmaps
     plot_correlation_heatmap(samples)
     plot_correlation_heatmap(pca_samples)
     plot_correlation_heatmap(z_normalized_samples)
-    
+
     # Plot histogram for all features
     plot_features_histograms(features, samples, labels)
-    
+
     # Scatterplots for all pairs of different features
     plot_feature_pairs_sctterplots(features, samples, labels)
